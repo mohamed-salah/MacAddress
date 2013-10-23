27c022430411795edb7b2efa532be4c82a6ce9cf
Mohamed Salah <mohamed.salah@badrit.com>
2013-10-23 11:52:19 +0200

    fix java file

--- src/android/MacAddressPlugin.java
+++ src/android/MacAddressPlugin.java
@@ -1,6 +1,5 @@
 package com.phonegap.plugins;
 
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
@@ -16,19 +15,22 @@ import android.net.wifi.WifiManager;
  */
 public class MacAddressPlugin extends CordovaPlugin {
 
-    @Override
     public boolean isSynch(String action) {
         if (action.equals("getMacAddress")) {
             return true;
         }
         return false;
     }
-    
-    /* (non-Javadoc)
-     * @see org.apache.cordova.api.Plugin#execute(java.lang.String, org.json.JSONArray, java.lang.String)
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.cordova.api.Plugin#execute(java.lang.String,
+     * org.json.JSONArray, java.lang.String)
      */
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+    public boolean execute(String action, JSONArray args,
+            CallbackContext callbackContext) {
 
         if (action.equals("getMacAddress")) {
 
@@ -38,21 +40,24 @@ public class MacAddressPlugin extends CordovaPlugin {
                 JSONObject JSONresult = new JSONObject();
                 try {
                     JSONresult.put("mac", macAddress);
-                    PluginResult r = new PluginResult(PluginResult.Status.OK, JSONresult);
+                    PluginResult r = new PluginResult(PluginResult.Status.OK,
+                            JSONresult);
                     callbackContext.success(macAddress);
                     r.setKeepCallback(true);
                     callbackContext.sendPluginResult(r);
                     return true;
                 } catch (JSONException jsonEx) {
-                    PluginResult r = new PluginResult(PluginResult.Status.JSON_EXCEPTION);
+                    PluginResult r = new PluginResult(
+                            PluginResult.Status.JSON_EXCEPTION);
                     callbackContext.error("error");
                     r.setKeepCallback(true);
                     callbackContext.sendPluginResult(r);
                     return true;
                 }
             }
-            return false;
         }
+        return false;
+    }
 
     /**
      * Gets the mac address.
@@ -61,7 +66,7 @@ public class MacAddressPlugin extends CordovaPlugin {
      */
     private String getMacAddress() {
         String macAddress = null;
-        WifiManager wm = (WifiManager) this.ctx.getSystemService(Context.WIFI_SERVICE);
+        WifiManager wm = (WifiManager) this.cordova.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         macAddress = wm.getConnectionInfo().getMacAddress();
 
         if (macAddress == null || macAddress.length() == 0) {
