1293eaf91c0b68b81bdfd3d854ece2973205de8c
Fábio Silva <fabio.fernando.osilva@gmail.com>
2016-06-21 17:30:19 -0300

    Android: add support to >= Marshmallow

--- src/android/MacAddressPlugin.java
+++ src/android/MacAddressPlugin.java
@@ -9,6 +9,11 @@ import org.json.JSONObject;
 
 import android.content.Context;
 import android.net.wifi.WifiManager;
+import android.os.Build;
+
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * The Class MacAddressPlugin.
@@ -65,14 +70,66 @@ public class MacAddressPlugin extends CordovaPlugin {
      * @return the mac address
      */
     private String getMacAddress() {
+
+        if (Build.VERSION.SDK_INT >= 23) { // Build.VERSION_CODES.M
+            return getMMacAddress();
+        }
+        
+        return getLegacyMacAddress();
+
+    }
+
+    /**
+     * Gets the mac address on version < Marshmallow.
+     * 
+     * @return the mac address
+     */
+    private String getLegacyMacAddress() {
+
         String macAddress = null;
+
         WifiManager wm = (WifiManager) this.cordova.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         macAddress = wm.getConnectionInfo().getMacAddress();
 
         if (macAddress == null || macAddress.length() == 0) {
-            macAddress = "00:00:00:00:00:00";
+            macAddress = "02:00:00:00:00:00";
         }
 
         return macAddress;
+
+    }
+
+    /**
+     * Gets the mac address on version >= Marshmallow.
+     * 
+     * @return the mac address
+     */
+    private String getMMacAddress() {
+
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02x", (b & 0xFF)) + ":");
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+
+                return res1.toString();
+            }
+        } catch (Exception ex) { }
+
+        return "02:00:00:00:00:00";
     }
 }
